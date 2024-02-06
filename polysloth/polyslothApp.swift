864382fd56c27699f5723c4885227e82ed9e07b5
Denis Ronchese <denis.ronchese@gmail.com>
2024-02-06 19:24:15 +0100

    Initial Commit

---
+++ polysloth/polyslothApp.swift
@@ -0,0 +1,17 @@
+//
+//  polyslothApp.swift
+//  polysloth
+//
+//  Created by Denis Ronchese on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct polyslothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
